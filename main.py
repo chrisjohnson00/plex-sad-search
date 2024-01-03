7d565e0d6567d8b600d6eadd1efcfea09e29d316
Chris Johnson <chrisjohnson00@hotmail.com>
2024-01-02 18:01:34 -0800

    feat: storing in redis (#13)
    * chore: bump python from 3.11-slim to 3.12-slim
    
    Bumps python from 3.11-slim to 3.12-slim.
    
    ---
    updated-dependencies:
    - dependency-name: python
      dependency-type: direct:production
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    * chore: bump actions/github-script from 6.4.1 to 7.0.1
    
    Bumps [actions/github-script](https://github.com/actions/github-script) from 6.4.1 to 7.0.1.
    - [Release notes](https://github.com/actions/github-script/releases)
    - [Commits](https://github.com/actions/github-script/compare/v6.4.1...v7.0.1)
    
    ---
    updated-dependencies:
    - dependency-name: actions/github-script
      dependency-type: direct:production
      update-type: version-update:semver-major
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    * chore: bump actions/setup-python from 4 to 5
    
    Bumps [actions/setup-python](https://github.com/actions/setup-python) from 4 to 5.
    - [Release notes](https://github.com/actions/setup-python/releases)
    - [Commits](https://github.com/actions/setup-python/compare/v4...v5)
    
    ---
    updated-dependencies:
    - dependency-name: actions/setup-python
      dependency-type: direct:production
      update-type: version-update:semver-major
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    * chore: bump actions/checkout from 3 to 4
    
    Bumps [actions/checkout](https://github.com/actions/checkout) from 3 to 4.
    - [Release notes](https://github.com/actions/checkout/releases)
    - [Changelog](https://github.com/actions/checkout/blob/main/CHANGELOG.md)
    - [Commits](https://github.com/actions/checkout/compare/v3...v4)
    
    ---
    updated-dependencies:
    - dependency-name: actions/checkout
      dependency-type: direct:production
      update-type: version-update:semver-major
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    * chore: bump actions/cache from 3.3.1 to 3.3.2
    
    Bumps [actions/cache](https://github.com/actions/cache) from 3.3.1 to 3.3.2.
    - [Release notes](https://github.com/actions/cache/releases)
    - [Changelog](https://github.com/actions/cache/blob/main/RELEASES.md)
    - [Commits](https://github.com/actions/cache/compare/v3.3.1...v3.3.2)
    
    ---
    updated-dependencies:
    - dependency-name: actions/cache
      dependency-type: direct:production
      update-type: version-update:semver-patch
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    * feat: storing in redis
    
    * style: lint fix
    
    * ci: concurrency on ci
    
    * feat: enriching with metadata from tmbd
    
    * feat: hydrating results with TMDB metadata
    
    * style: lint fixes
    
    * style: lint fixes
    
    * chore: update config cache to 7 days
    
    * refactor: move redis/tmdb libraries to shared location
    
    * style: lint fix
    
    ---------
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>

--- main.py
+++ main.py
@@ -1,16 +1,23 @@
 import inspect
 import os
 from datetime import datetime, timedelta
+import logging
 
 from plexapi.server import PlexServer
 from plexapi.video import Movie
+import sad_libraries.redis as sad_redis
+import sad_libraries.tmdb as sad_tmdb
 
 
 def main():
-    horror_movies()
+    search_keys = []
+    results_to_store = {}
+    search_keys, results_to_store = horror_movies(search_keys=search_keys, results_to_store=results_to_store)
+    sad_redis.save_to_cache(key='sad_search_keys', data=search_keys)
+    sad_redis.save_to_cache(key='sad_results', data=results_to_store)
 
 
-def horror_movies():
+def horror_movies(*, search_keys, results_to_store):
     """
     Get all movies with a genre of Horror which are unwatched, added to the library more than 90 days ago
     :return:
@@ -25,23 +32,40 @@ def horror_movies():
         if any(genre.tag.lower() == "horror" for genre in movie.genres)
     ]
     size = 0
+    count = 0
     # Print unwatched horror movies and calculate their total size
     for movie in horror_movie_results:
         # exclude movies added in the last 90 days
         if movie.addedAt < ninety_days_ago:
-            print(
-                f"Movie: {movie.title}, File path: {sanitize_file_path(movie.media[0].parts[0].file)}, "
-                f"Rating Key: {movie.ratingKey}")
+            logger.info(
+                f"Movie: '{movie.title}', File path: '{sanitize_file_path(movie.media[0].parts[0].file)}'")
             size += movie.media[0].parts[0].size  # Increment size with the size of the movie
-            store_movie(movie=movie, search_key=inspect.currentframe().f_code.co_name)
+            count += 1
+            store_movie(movie=movie, search_key=inspect.currentframe().f_code.co_name,
+                        results_to_store=results_to_store)
     size_gb = size / (1024 ** 3)  # convert size to gigabytes
-    print(f"Total size of horror movies: {size_gb}GB")
+    logger.info(f"Total size of {inspect.currentframe().f_code.co_name}: {size_gb}GB, {count} movies")
+    search_keys.append(inspect.currentframe().f_code.co_name)
+    return search_keys, results_to_store
 
 
-def store_movie(*, movie: Movie, search_key: str):
-    movie_dict = {"title": movie.title, "file_path": sanitize_file_path(movie.media[0].parts[0].file),
-                  "id": movie.ratingKey, "size_bytes": movie.media[0].parts[0].size, "search_key": search_key}
-    eligible_movies.append(movie_dict)
+def store_movie(*, movie: Movie, search_key: str, results_to_store: dict):
+    tmdb_results = sad_tmdb.search_movie_by_query_and_year(query=movie.title, year=movie.year)
+    # tmdb_results comes back with a dict of this format:
+    # {'page': 1, 'results': [], 'total_pages': 1, 'total_results': 0}
+    if tmdb_results['total_results'] > 0:
+        movie_dict = {"file_path": sanitize_file_path(movie.media[0].parts[0].file), "id": movie.ratingKey,
+                      "size_bytes": movie.media[0].parts[0].size, "tmdb_results": tmdb_results['results'][0],
+                      "audience_rating": movie.audienceRating}
+        if search_key in results_to_store:
+            results_to_store[search_key].append(movie_dict)
+        else:
+            results_to_store[search_key] = [movie_dict]
+    else:
+        logger.error(
+            f"TMDB API did not return any results for '{movie.title} ({movie.year})' with a file name "
+            f"of '{sanitize_file_path(movie.media[0].parts[0].file)}'")
+    return results_to_store
 
 
 def sanitize_file_path(file_path):
@@ -63,7 +87,13 @@ def get_plex_client():
     return plex
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    eligible_movies = []
+    logger = logging.getLogger("sad")
+    logger.setLevel(os.environ.get("SAD_LOG_LEVEL", "INFO"))
+    logger.handlers = []
+    console_handler = logging.StreamHandler()
+    console_handler.setLevel(os.environ.get("SAD_LOG_LEVEL", "INFO"))
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    console_handler.setFormatter(formatter)
+    logger.addHandler(console_handler)
     main()
